5645e699728e05ae3636495eaf889d9c9e0d58ac
oompaloompa471 <fillipyifmg@gmail.com>
2024-11-21 21:23:58 -0300

    chore:added test environment to backend

---
+++ backend/src/controllers/tests/user.controller.spec.ts
@@ -0,0 +1,7 @@
+describe("User controller tests", () => {
+  it("teste", () => {
+    const first = 3;
+    const second = 4;
+    expect(first + second).toEqual(first + second);
+  });
+});
---
+++ backend/src/controllers/user.controller.ts
@@ -0,0 +1,20 @@
+import { Request, Response } from "express";
+
+export class UserController {
+    
+  public static async createUser(req: Request, res: Response) {
+    try {
+      return res.status(200).send("Usuário criado com sucesso!");
+    } catch (error) {
+      return res.status(500).send("Erro ao criar usuário: " + error.message);
+    }
+  }
+
+  public static async getAllUsers(req: Request, res: Response) {
+    try {
+      return res.status(200).send("Usuários retornados com sucesso");
+    } catch (error) {
+      return res.status(500).send("Erro ao obter usuários: " + error.message);
+    }
+  }
+}
