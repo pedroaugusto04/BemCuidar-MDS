a6f4e02c1b752ab03923fbdd147213490151313e
oompaloompa471 <fillipyifmg@gmail.com>
2024-11-21 21:33:30 -0300

    feat:added icons to menu

--- frontend/src/app/components/list-provider/list-provider.component.spec.ts
+++ frontend/src/app/components/list-provider/list-provider.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from "@angular/core/testing";
-
 import { ListComponent } from "./list-provider.component";
+import { ServiceProvider } from "../../models/ServiceProvider";
+import { By } from "@angular/platform-browser";
 
 describe("ListComponent", () => {
   let component: ListComponent;
@@ -19,4 +20,53 @@ describe("ListComponent", () => {
   it("should create", () => {
     expect(component).toBeTruthy();
   });
+
+  it("should create", () => {
+    expect(component).toBeTruthy();
+  });
+
+  it("Should render providers correctly in app card", () => {
+
+    const t1: ServiceProvider = {
+      name: "Nome1",
+      service: "Work1",
+      age: 20,
+      country: "Brasil",
+      state: "Minas Gerais",
+      city: "Ipatinga",
+    };
+    const t2: ServiceProvider = {
+      name: "Nome2",
+      service: "Work2",
+      age: 21,
+      country: "Brasil",
+      state: "Minas Gerais",
+      city: "Ipatinga",
+    };
+    const t3: ServiceProvider = {
+      name: "Nome3",
+      service: "Work3",
+      age: 22,
+      country: "Inglaterra",
+      state: "Minas Gerais",
+      city: "Ipatinga",
+    };
+    component.providers = [t1,t2,t3];
+    fixture.detectChanges();
+
+    const cardProviderElements = fixture.debugElement.queryAll(By.css("app-card-service-provider"));
+
+    expect(cardProviderElements.length).toBe(component.providers.length);
+    cardProviderElements.forEach((element, i) => {
+    const provider = component.providers[i];
+    expect(element.componentInstance.providerName).toBe(provider.name);
+    expect(element.componentInstance.providerService).toBe(provider.service);
+    expect(Number(element.componentInstance.providerAge)).toBe(provider.age);
+    expect(element.componentInstance.providerCountry).toBe(provider.country);
+    expect(element.componentInstance.providerState).toBe(provider.state);
+    expect(element.componentInstance.providerCity).toBe(provider.city);
+    });
+
+  });
+  
 });
--- frontend/src/app/components/nav-bar/nav-bar.component.spec.ts
+++ frontend/src/app/components/nav-bar/nav-bar.component.spec.ts
@@ -1,23 +1,80 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from "@angular/core/testing";
 
-import { NavBarComponent } from './nav-bar.component';
+import { NavBarComponent } from "./nav-bar.component";
 
-describe('NavBarComponent', () => {
+describe("NavBarComponent", () => {
   let component: NavBarComponent;
   let fixture: ComponentFixture<NavBarComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [NavBarComponent]
-    })
-    .compileComponents();
-    
+      imports: [NavBarComponent],
+    }).compileComponents();
+
     fixture = TestBed.createComponent(NavBarComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  it("Should render if desktop", () => {
+    component.isMobile = false;
+    fixture.detectChanges();
+
+    const sections = fixture.nativeElement.querySelector("#sections-desktop");
+
+    const divProfileImg = fixture.nativeElement.querySelector(
+      "#profile-img-desktop"
+    );
+
+    expect(sections).not.toBeNull();
+    expect(divProfileImg).not.toBeNull();
+  });
+
+  it("Should NOT render if desktop", () => {
+    component.isMobile = false;
+    fixture.detectChanges();
+
+    const buttonElement = fixture.nativeElement.querySelector(
+      "#button-menu-mobile"
+    );
+
+    const profileImg = fixture.nativeElement.querySelector(
+      "#profile-img-mobile"
+    );
+
+    expect(buttonElement).toBeNull();
+
+    expect(profileImg).toBeNull();
+  });
+
+  it("Should render if mobile", () => {
+    component.isMobile = true;
+    fixture.detectChanges();
+
+    const buttonElement = fixture.nativeElement.querySelector(
+      "#button-menu-mobile"
+    );
+
+    const profileImg = fixture.nativeElement.querySelector(
+      "#profile-img-mobile"
+    );
+
+    expect(buttonElement).not.toBeNull();
+
+    expect(profileImg).not.toBeNull();
+  });
+
+  it("Should NOT render if mobile", () => {
+    component.isMobile = true;
+    fixture.detectChanges();
+
+    const sections = fixture.nativeElement.querySelector("#sections-desktop");
+
+    const divProfileImg = fixture.nativeElement.querySelector(
+      "#profile-img-desktop"
+    );
+
+    expect(sections).toBeNull();
+    expect(divProfileImg).toBeNull();
   });
 });
