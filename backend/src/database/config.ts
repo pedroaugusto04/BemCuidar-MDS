fe1d24f7d3d79a166fe838022c571f94034efe02
pedroaugusto04 <pedroaugustoaduarte@gmail.com>
2024-11-24 12:44:32 -0300

    chore: configuracao do banco de producao

--- backend/src/database/config.ts
+++ backend/src/database/config.ts
@@ -5,20 +5,26 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-export const connection = new Pool({
-  host: process.env.POSTGRES_HOST_DEV,
-  port: parseInt(process.env.POSTGRES_PORT_DEV || "5434"),
-  user: process.env.POSTGRES_USER_DEV,
-  password: process.env.POSTGRES_PASSWORD_DEV,
-  database: process.env.POSTGRES_DB_DEV,
-});
+const poolConfig =
+  process.env.NODE_ENV === "production"
+    ? {
+        // PRODUCAO
+        host: process.env.POSTGRES_HOST,
+        port: parseInt(process.env.POSTGRES_PORT || "5432"),
+        user: process.env.POSTGRES_USER,
+        password: process.env.POSTGRES_PASSWORD,
+        database: process.env.POSTGRES_DB,
+        ssl: {
+          rejectUnauthorized: false,
+        },
+      }
+    : {
+        // DESENVOLVIMENTO
+        host: process.env.POSTGRES_HOST_DEV,
+        port: parseInt(process.env.POSTGRES_PORT_DEV || "5434"),
+        user: process.env.POSTGRES_USER_DEV,
+        password: process.env.POSTGRES_PASSWORD_DEV,
+        database: process.env.POSTGRES_DB_DEV,
+      };
 
-/*
-prod
-export const connection = createPool({
-  host: process.env.MYSQL_HOST,
-  port: parseInt(process.env.MYSQL_PORT || "3306"),
-  user: process.env.MYSQL_USERNAME,
-  password: process.env.MYSQL_PASSWORD,
-  database: process.env.MYSQL_DATABASE,
-});*/
+export const connection = new Pool(poolConfig);
