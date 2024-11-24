b54bbe67b7588d2f69555323b106019432378b13
pedroaugusto04 <pedroaugustoaduarte@gmail.com>
2024-11-23 22:23:00 -0300

    fix: correcoes para deploy front

---
+++ frontend/dist/mds/server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-UDITSVQO.mjs";import"./chunk-2XD62IC7.mjs";import"./chunk-QKQ6F3OK.mjs";import"./chunk-J4OVULGT.mjs";import"./chunk-T7GZVMYR.mjs";import"./chunk-VVCT4QZE.mjs";export{a as default};
