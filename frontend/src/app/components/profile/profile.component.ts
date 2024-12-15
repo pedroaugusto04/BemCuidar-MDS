4c21946d9a158f904b570c36bcd30e2acb3b8fa2
oompaloompa471 <felipesilva2014922@gmail.com>
2024-12-14 21:49:18 -0300

    navbar-responsive-correction

--- frontend/src/app/components/profile/profile.component.ts
+++ frontend/src/app/components/profile/profile.component.ts
@@ -2,13 +2,15 @@ import { Component } from "@angular/core";
 import { NavBarComponent } from "../nav-bar/nav-bar.component";
 import { MatIcon } from "@angular/material/icon";
 import { RouterLink } from "@angular/router";
+import {MatButtonModule} from '@angular/material/button';
 
 @Component({
   selector: "app-profile",
   standalone: true,
-  imports: [NavBarComponent, MatIcon, RouterLink],
+  imports: [NavBarComponent, MatButtonModule, MatIcon, RouterLink],
   templateUrl: "./profile.component.html",
   styleUrl: "./profile.component.scss",
+  
 })
 export class ProfileComponent {
   constructor() {}
